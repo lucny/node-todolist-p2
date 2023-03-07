69a8a11da69c770a900002abb914d768964796d8
lucny <lucny@sspu-opava.cz>
2023-03-07 10:10:19 +0100

    03 - formular v HTML

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require("express");
 const app = express();
 /* Nastavení portu, na němž bude spuštěný server naslouchat */ 
 const port = 3000;
+
+/* Identifikace složky obsahující statické soubory klientské části webu */ 
+app.use(express.static("public"));
+
 /* Spuštění webového serveru */ 
 app.listen(port, () => {
   console.log(`Server naslouchá na portu ${port}`);

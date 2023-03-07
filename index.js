105f6758cba2009dc0e1b6cacbe2250cbdf389d5
lucny <lucny@sspu-opava.cz>
2023-03-07 10:18:24 +0100

    04 - instalace knihoven body-parser, moment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 /* Připojení modulu frameworku Express */
 const express = require("express");
+/* Připojení externího modulu body-parser (https://www.npmjs.com/package/body-parser) - middleware pro parsování těla požadavku */ 
+const bodyParser = require("body-parser"); 
+/* Připojení externího modulu moment (https://momentjs.com/) - knihovna pro formátování datových a časových údajů */ 
+const moment = require("moment"); 
+/* Připojení vestavěných modulů fs (práce se soubory) a path (cesty v adresářové struktuře) */ 
+const fs = require("fs"); 
+const path = require("path");
+
 /* Vytvoření základního objektu serverové aplikace */ 
 const app = express();
 /* Nastavení portu, na němž bude spuštěný server naslouchat */ 

ec110965bb5ec3e3540bd0646e4e688c14a7dc43
lucny <lucny@sspu-opava.cz>
2023-03-21 08:40:05 +0100

    03-PUG

--- index.js
+++ index.js
@@ -15,6 +15,37 @@ const port = 3000;
 
 /* Identifikace složky obsahující statické soubory klientské části webu */ 
 app.use(express.static("public"));
+/* Nastavení typu šablonovacího engine na pug*/ 
+app.set("view engine", "pug"); 
+/* Nastavení složky, kde budou umístěny šablony pug */ 
+app.set("views", path.join(__dirname, "views"));
+
+/* Využití modulu body-parser pro parsování těla požadavku */ 
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+let datumZadani = moment().format('YYYY-MM-DD');
+app.post('/savedata', urlencodedParser, function(req, res) {
+  let data = `"${req.body.ukol}","${req.body.predmet}","${datumZadani}","${req.body.odevzdani}"\n`;
+  fs.appendFile(path.join(__dirname, 'data/ukoly.csv'), data, function(err) {
+    if (err) {
+      console.log('Nastala chyba: ', err);
+      return res.status(400).json({
+        success: false,
+        message: 'Nastala chyba při zápisu do souboru!'
+      });
+    };
+    res.redirect(301, '/');
+  });
+})
+
+app.get('/todolist', function(req, res) {
+  fs.readFile(path.join(__dirname, 'data/ukoly.csv'), function(err, data) {
+    res.send(data);
+  });
+});
+
+app.get('/pokus', function(req, res) {
+  res.render('index', {nadpis: 'Seznam úkolů'});
+})
 
 /* Spuštění webového serveru */ 
 app.listen(port, () => {
